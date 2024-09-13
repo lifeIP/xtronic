fa1cf30914b01014486fc16df98543c19737ddd6
lifeIP <molokeevartem@gmail.com>
2024-09-14 00:29:25 +0700

    completion of main work

--- src/components/MySlides/MySlides.tsx
+++ src/components/MySlides/MySlides.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Card, CardActions, CardContent, FormControlLabel, IconButton, Slide, Switch, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, FormControlLabel, Grid, IconButton, Slide, Switch, Typography } from '@mui/material';
 
 import { styled } from '@mui/system';
 import { useTimer, TimerSettings } from 'react-timer-hook';
@@ -23,38 +23,28 @@ export default function MySlides() {
 
   const [b_nav_disabled_l, setBNavDisabled_l] = React.useState<boolean>(false)
   const [b_nav_disabled_r, setBNavDisabled_r] = React.useState<boolean>(false)
-  const [b_nav_disabled_u, setBNavDisabled_u] = React.useState<boolean>(false)
-  const [b_nav_disabled_d, setBNavDisabled_d] = React.useState<boolean>(false)
 
 
   const [m_map, setMmap] = React.useState<number[][]>([[],])
-  // let m_map: number[][] = [[]]
-  const [pagePosition, setPagePosition] = React.useState({ x: 5, y: 1 });
+
+  const [start_index, set_start_index] = React.useState(0)
+  const [end_index, set_end_index] = React.useState(9)
+  
+  const [pagePosition, setPagePosition] = React.useState({ x: 1, y: 1 });
 
 
   useEffect(() => {
     // Получение карты сайта 
     axios.get(conf.addr + "/" + "map").then(
       (res) => {
-        // console.log(res.data.map[0]);
         setMmap(res.data.map);
-        // console.log(m_map);
-
-        setBNavDisabled_l(res.data.map.at(pagePosition.y)?.at(pagePosition.x-1) == 0);
-        setBNavDisabled_r(res.data.map.at(pagePosition.y)?.at(pagePosition.x+1) == 0);
-
-        setBNavDisabled_u(res.data.map.at(pagePosition.y-1)?.at(5) == 0);
-        setBNavDisabled_d(res.data.map.at(pagePosition.y+1)?.at(5) == 0);
+        set_start_index(res.data.start)
+        set_end_index(res.data.end)
       }
     ).catch((err) => {
       console.log("Error: ", err)
     })
 
-    // if(m_map.at(pagePosition.y+1)?.at(pagePosition.x) == 0) b_nav.u = true;
-    // else b_nav.u = false;
-    // if(m_map.at(pagePosition.y-1)?.at(pagePosition.x) == 0) b_nav.d = true;
-    // else b_nav.d = false;
-    // Получение карты сайта 
   }, [])
 
 
@@ -69,25 +59,12 @@ export default function MySlides() {
     onExpire: () => {
 
       setChecked(true);
-      if (direction == "down") {
-        setDirection("up");
-        setPagePosition({ x: 5, y: pagePosition.y + 1 });
-      }
-      else if (direction == "up") {
-        setDirection("down");
-        setPagePosition({ x: 5, y: pagePosition.y - 1 });
-      }
-      else if (direction == "left") {
-        if (m_map[pagePosition.y][pagePosition.x - 1] == 0) {
-          let last_index = pagePosition.x;
-
-          m_map[pagePosition.y].forEach((e: number, index: number) => {
-            if (e != 0) last_index = index;
-          });
-
+      
+      if (direction == "left") {
+        if (pagePosition.x - 1 < start_index) {
           
           setDirection("right");
-          setPagePosition({ x: last_index, y: pagePosition.y })
+          setPagePosition({ x: end_index, y: pagePosition.y })
         }
         else {
           setDirection("right");
@@ -95,19 +72,9 @@ export default function MySlides() {
         }
       }
       else if (direction == "right") {
-        if (m_map.at(pagePosition.y)?.at(pagePosition.x + 1) == 0) {
-          let first_index = pagePosition.x;
-          let s_flag = true;
-          m_map[pagePosition.y].forEach((e: number, index: number) => {
-            if (e != 0 && s_flag) {
-              first_index = index;
-              s_flag = false;
-            }
-
-          });
-
+        if (pagePosition.x + 1 > end_index) {
           setDirection("left");
-          setPagePosition({ x: first_index, y: pagePosition.y })
+          setPagePosition({ x: start_index, y: pagePosition.y })
         }
         else {
           setDirection("left");
@@ -124,18 +91,6 @@ export default function MySlides() {
     v_time.setSeconds(v_time.getSeconds() + time_in_seconds);
     restart(v_time, true)
   }
-
-  useEffect(() => {
-
-    console.log(pagePosition)
-
-
-    // setBNavDisabled_l(m_map.at(pagePosition.y)?.at(pagePosition.x-1) == 0);
-    // setBNavDisabled_r(m_map.at(pagePosition.y)?.at(pagePosition.x+1) == 0);
-
-    setBNavDisabled_u(m_map.at(pagePosition.y - 1)?.at(5) == 0);
-    setBNavDisabled_d(m_map.at(pagePosition.y + 1)?.at(5) == 0);
-  }, [pagePosition]);
 
   useEffect(() => {
     console.log(flag)
@@ -168,18 +123,17 @@ export default function MySlides() {
       <Box sx={{ translate: "15% 10%", width: "80vw", height: "85vh", position: "absolute" }}>
         <Box sx={{ display: "inline-flex", justifyContent: "center", alignItems: "center" }}>
           <IconButton sx={{ height: "60px", width: "60px" }} size="large" disabled={b_nav_disabled_l} onClick={() => { changeDirection("left") }}><KeyboardArrowLeftIcon sx={{ height: "50px", width: "50px" }} /></IconButton>
-
-          <Box>
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-              <IconButton sx={{ height: "60px", width: "60px" }} size="large" disabled={b_nav_disabled_u} onClick={() => { changeDirection("up") }}><KeyboardArrowUpIcon sx={{ height: "50px", width: "50px" }} /></IconButton>
-            </Box>
-            <Box sx={{ width: "70vw", height: "70vh" }}>
+          <Grid container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ minHeight: '75vh' }}>
+                        <Grid item xs={1}>
+            <Box sx={{ width: "70vw", height: "70vh"}}>
               <PageWorker pagePosition={pagePosition} pageIndexes={m_map}/>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-              <IconButton sx={{ height: "60px", width: "60px" }} size="large" disabled={b_nav_disabled_d} onClick={() => { changeDirection("down") }}><KeyboardArrowDownIcon sx={{ height: "50px", width: "50px" }} /></IconButton>
-            </Box>
-          </Box>
+            </Grid>
+            </Grid>
           <IconButton sx={{ height: "60px", width: "60px" }} size="large" disabled={b_nav_disabled_r} onClick={() => { changeDirection("right") }}><KeyboardArrowRightIcon sx={{ height: "50px", width: "50px" }} /></IconButton>
         </Box>
       </Box>
--- src/components/Pages/PageWorker.tsx
+++ src/components/Pages/PageWorker.tsx
@@ -1,11 +1,20 @@
-import { Box, Card, CardContent, Grid2, Typography } from '@mui/material'
+import { Box, Card, CardContent, Grid, Grid2, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import HelloPage from './HelloPage'
+
 import back_1 from './img/1.png'
+import back_2 from './img/2.png'
+import back_3 from './img/3.png'
+import back_4 from './img/4.png'
+
+import perehodnic from './img/perehodnic.png'
+import perehodnic_2 from './img/perehodnic_2.png'
+
+import './PageWorker.css'
 
 export default function PageWorker({ pagePosition, pageIndexes }: { pagePosition: { x: number, y: number }, pageIndexes: number[][] }) {
     const [page, setPage] = useState<number | undefined>(1);
-    
+
     useEffect(() => {
         let index = pageIndexes.at(pagePosition.y)?.at(pagePosition.x);
         setPage(index)
@@ -16,37 +25,222 @@ export default function PageWorker({ pagePosition, pageIndexes }: { pagePosition
             backgroundImage: `url(${back_1})`,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
-          };
+        };
         return (
             <Box>
-                <Card style={myStyle} sx={{borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                <Card style={myStyle} sx={{ borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <Grid container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ minHeight: '75vh' }}>
+                        <Grid item xs={1}>
+                            <Typography textAlign="center" variant='h2' color='#FFFFFF'>
+                                Переходники для быстрой зарядки электрмобилей
+                            </Typography>
+
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Box >
+        )
+    }
+
+
+
+    if (page == 2) {
+        return (
+            <Box>
+                <Card sx={{ opacity: .85, backgroundColor: "#1E252E", borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
                     <CardContent>
-                        <Typography textAlign="center" variant='h2' color='#FFFFFF'>
-                            Переходники для быстрой зарядки электрмобилей
+                        <Typography textAlign="center" variant='h4' color='#FFFFFF'>
+                            Переходник для быстрой зарядки CCS-GB/T
                         </Typography>
                     </CardContent>
+                    <Box sx={{ display: "inline-flex" }}>
+                        <Box className='perehodnic_1' sx={{ width: "500px", height: "400px" }} />
+                        <Box sx={{ width: "35vw", marginTop: "15vh" }}>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Мощность: от 150 кВт-200кВт
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Максимальный ток: до 250 А
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Кол-во фаз: 3 (три)
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Рабочая температура: -30 до +50
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Ном. напряжение: до 1000В
+
+                            </Typography>
+                        </Box>
+                    </Box>
+
                 </Card>
             </Box>
         )
     }
-    if (page == 2) {
+
+
+
+    if (page == 3) {
         return (
             <Box>
-                <Card sx={{ opacity: .85, backgroundColor: "#1E252E", borderRadius: "36.5px", width: "70vw", height: "65vh" }}>
-                    <Typography variant='h2'>
-                        ewfcewirhfauwehdhowaodjoi
-                    </Typography>
+                <Card sx={{ opacity: .85, backgroundColor: "#1E252E", borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <CardContent>
+                        <Typography textAlign="center" variant='h4' color='#FFFFFF'>
+                            Переходник для быстрой зарядки CHAdeMO - GB/T
+                        </Typography>
+
+                    </CardContent>
+                    <Box sx={{ display: "inline-flex" }}>
+                        <Box className='perehodnic_2' sx={{ width: "500px", height: "400px" }} />
+                        <Box sx={{ width: "35vw", marginTop: "15vh" }}>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Мощность:  до 60 кВт
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Максимальный ток: до 120 А
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Кол-во фаз: 3 (три)
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Рабочая температура: -30 до +50
+                            </Typography>
+                        </Box>
+                    </Box>
+
                 </Card>
             </Box>
+        )
+    }
+
+
+    if (page == 4) {
+        return (
+            <Box>
+                <Card sx={{ opacity: .85, backgroundColor: "#1E252E", borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <CardContent>
+                        <Typography textAlign="center" variant='h4' color='#FFFFFF'>
+                            Переходник для быстрой зарядки CHAdeMO - GB/T для VW ID
+                        </Typography>
+                    </CardContent>
+                    <Box sx={{ display: "inline-flex" }}>
+                        <Box className='perehodnic_3' sx={{ width: "500px", height: "400px" }} />
+                        <Box sx={{ width: "35vw", marginTop: "15vh" }}>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Мощность:  до 60 кВт
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Максимальный ток: до 120 А
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Кол-во фаз: 3 (три)
+                            </Typography>
+                            <Typography variant='h4' color='#FFFFFF'>
+                                Рабочая температура: -30 до +50
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Card>
+            </Box>
+        )
+    }
+
+
+
+    if (page == 5) {
+        const myStyle = {
+            backgroundImage: `url(${back_2})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+        };
+        return (
+            <Box>
+                <Card style={myStyle} sx={{ borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <Grid container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ minHeight: '75vh' }}>
+                        <Grid item xs={1}>
+                            <Typography textAlign="center" variant='h2' color='#FFFFFF'>
+                                Зарядные станции для зарядки электромобилей
+                            </Typography>
+
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Box >
+        )
+    }
+
+
+    if (page == 6) {
+        const myStyle = {
+            backgroundImage: `url(${back_3})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+        };
+        return (
+            <Box>
+                <Card style={myStyle} sx={{ borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <Grid container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ minHeight: '75vh' }}>
+                        <Grid item xs={1}>
+                            <Typography textAlign="center" variant='h2' color='#FFFFFF'>
+                            Мы изготавливаем зарядные станции в соответствии с техническим заданием и требованиями каждого клиента
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Box >
+        )
+    }
+
+
+
+    if (page == 7) {
+        const myStyle = {
+            backgroundImage: `url(${back_4})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+        };
+        return (
+            <Box>
+                <Card style={myStyle} sx={{ borderRadius: "36.5px", width: "70vw", height: "70vh" }}>
+                    <Grid container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ minHeight: '75vh' }}>
+                        <Grid item xs={1}>
+                            <Typography textAlign="center" variant='h2' color='#FFFFFF'>
+                                Программное обеспечение для переходника CCS - GB/T
+                            </Typography>
+
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Box >
         )
     }
     return (
         <Box>
             <Card sx={{ opacity: .85, backgroundColor: "#1E252E", borderRadius: "36.5px", width: "70vw", height: "65vh" }}>
-                
+
                 <Typography variant='h2'>
                     {/* Enother */}
                 </Typography>
+
             </Card>
         </Box>
     )
